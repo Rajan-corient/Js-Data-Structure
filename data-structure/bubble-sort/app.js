697fb4f99fbf23e22732adbe8654ad823b530457
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2022-09-21 20:54:54 +0530

    bubble sort changes

--- data-structure/bubble-sort/app.js
+++ data-structure/bubble-sort/app.js
@@ -1,8 +1,31 @@
 // Bubble Sort Algorithm
 // A sorting algorithm where largest value bubles up at the top
+// [4,3,2,1]   n=4, index=3
+// no of passes n-1=3
+
+
+// [4,3,2,1]
+// pass1
+// [3,4,2,1]    4>3  swap  
+// [3,2,4,1]    4>2  swap 
+// [3,2,1,4]    4>1  swap
+
+// pass2
+// [2,3,1,4]    3>2  swap
+// [2,1,3,4]    3>1  swap 
+// [2,1,3,4]    3<4  no swap
+
+// pass3
+// [2,1,3,4]    2>1  swap
+// [1,2,3,4]    4>2  swap 
+// [1,2,3,4]    2<3  no swap
+
+// Result => [1,2,3,4]
+
+
 
 function bubbleSort2(array) {
-    for (let i = array.length; i > 0; i--) {
+    for (let i = array.length-1; i > 0; i--) {
         for (let j = 0; j < i; j++) {
             console.log('outer loop', +i, 'inner loop', +j);
             if (array[j] > array[j+1]) {
@@ -14,7 +37,7 @@ function bubbleSort2(array) {
 }
 
 function bubbleSort(array) {
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length-1; i++) {
         isSwapped = false;
         for (let j = 0; j < array.length-i-1; j++) {
             console.log('outer loop', +i, 'inner loop', +j);
--- data-structure/recursion/app.js
+++ data-structure/recursion/app.js
@@ -38,7 +38,7 @@ function swap (arr, xp, yp) {
 }
 
 function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length-1; i++) {
         let isSwapped = false;
         for (let j = 0; j < arr.length-i-1; j++) {
             if (arr[j] > arr[j+1]) {
