41739f9f95fbbe14e273d4e7a862d31192bd1fab
Rajan Gupta <31067215+Rajan-corient@users.noreply.github.com>
2022-09-22 12:41:22 +0530

    selection sort desc added

--- data-structure/selection-sort/app.js
+++ data-structure/selection-sort/app.js
@@ -1,6 +1,13 @@
 // selection sort
 // [7,8,2,4,1,3]
 // first select min and then swap
+//  [7,8,2,4,1,3]
+//  pass1:  i=0   [1,8,2,4,7,3]
+//  pass2:  i=1   [1,2,8,4,7,3]
+//  pass3:  i=2   [1,2,3,4,7,8]
+//  pass4:  i=3   [1,2,3,4,7,8]
+//  pass5:  i=4   [1,2,3,4,7,8]
+
 
 function selectionSort(arr) {
     for (let i = 0; i < arr.length-1; i++) {
